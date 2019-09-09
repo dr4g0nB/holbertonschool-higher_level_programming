f56372929b2be5c9c3df00d2bc0b8ccc7e02c931
dabrabgellak <barbaracalle10i3@gmail.com>
2019-09-09 21:46:43 +0000

    rm the var strtotal to space

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-strtotal = (str1 + " " +str2)
-print("Welcome to {}!".format(strtotal))
+space = " "
+print("Welcome to {}!".format(str1+space+str2))
