d18e5133b9448ba916d292a9587aa0a54ff7e673
dabrabgellak <barbaracalle10i3@gmail.com>
2019-09-10 20:04:34 +0000

    works on positive

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,5 +7,5 @@ if number % 10 < 6 and number % 10 != 0:
     print ('Last digit of {:d}'.format(number), 'is {:d} is less than 6 and not 0'.format(absnumber * -1))
 elif number % 10 > 5:
     print('Last diigt of {:d}'.format(number), 'is {:d} and is greater than 5'.format(absnumber))
-elif:
+elif number % 10 == 0:
     print('Last digit of {:d}'.format(number), 'is {:d} and is 0'.format(absnumber))
