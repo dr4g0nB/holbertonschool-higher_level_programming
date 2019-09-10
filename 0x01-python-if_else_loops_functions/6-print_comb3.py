57912531263189e3da190ec2db5cd5b3abd89c32
dabrabgellak <barbaracalle10i3@gmail.com>
2019-09-10 23:42:44 +0000

    Prints digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+numb = 0
+while numb < 8:
+    num1 = numb
+    while num1 <= 9:
+        if numb != num1:
+            print('{:d}{:d}'.format(numb, num1), end=", ")
+            num1 += 1
+    numb += 1
+print('{:d}{:d}'.format(numb, num1 - 1), sep="")
