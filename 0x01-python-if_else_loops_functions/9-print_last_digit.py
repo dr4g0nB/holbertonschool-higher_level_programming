45b07385d4f6fdb70ebb36d127ad0dafa69b68e6
dabrabgellak <barbaracalle10i3@gmail.com>
2019-09-11 21:57:31 +0000

    Prints and return last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-#    if not (number is None):
-#        lasdig = abs(number) % 10
-#    return(lasdig)
-    lasdig = (abs(number) % 10) 
-    if number != 0:
-#        lasdig = (abs(number) % 10)
-        return(lasdig)
+    if number < 0:
+        number *= -1
+    print('{:d}'.format(number % 10), end="")
+    return number % 10
