d724732622322a8413747795287d4e390e0a1fec
dabrabgellak <barbaracalle10i3@gmail.com>
2019-09-12 22:28:17 +0000

    Prints the number of and the list og its args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    argc = len(sys.argv)
+
+    if argc == 1:
+        print('{:d} arguments'.format(argc - 1))
+    elif argc == 2:
+        print('{:d} argument'.format(argc - 1))
+        print('1: {}'.format(sys.argv[1]))
+    else:
+        print('{:d} arguments:'.format(argc - 1))
+        for i in range(argc):
+            if i != 0:
+                print('{:d}: {}'.format(i, sys.argv[i]))
