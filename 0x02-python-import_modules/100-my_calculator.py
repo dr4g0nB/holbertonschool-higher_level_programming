9994af9d9b520e97ea5fe639aab5750b942b1389
dabrabgellak <barbaracalle10i3@gmail.com>
2019-09-13 04:34:32 +0000

    Changed var

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,10 +4,10 @@ if __name__ == "__main__":
     import sys
 
     argc = len(sys.argv)
-
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     c = sys.argv[2]
+
     if argc != 4:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         exit(1)
@@ -15,12 +15,11 @@ if __name__ == "__main__":
     if c != '+' and c != '-' and c != '/' and c != '*':
         print('Unknown operator. Available operators: +, -, * and /')
         exit(1)
-
-    if sys.argv[2] == '+':
+    elif c == '+':
         print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
-    elif sys.argv[2] == '-':
+    elif c == '-':
         print('{:d} + {:d} = {:d}'.format(a, b, sub(a, b)))
-    elif sys.argv[2] == '*':
+    elif c == '*':
         print('{:d} + {:d} = {:d}'.format(a, b, mul(a, b)))
-    elif sys.argv[2] == '/':
+    elif c == '/':
         print('{:d} + {:d} = {:d}'.format(a, b, div(a, b)))
