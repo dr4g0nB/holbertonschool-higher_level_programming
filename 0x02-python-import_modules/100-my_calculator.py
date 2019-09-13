9efa97d23df4c207f995c32f7e083bcb8e020b08
dabrabgellak <barbaracalle10i3@gmail.com>
2019-09-13 02:13:58 +0000

    Doesn't do math

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,13 +9,17 @@ if __name__ == "__main__":
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         exit(1)
     else:
-        if len(sys.argv)[2] == '+':
-            print('{:d}'.format(a + b))
-        elif len(sys.argv)[2] == '-':
-            print('{:d}'.format(a - b))
-        elif len(sys.argv)[2] == '*':
+        if sys.argv[2] == '+':
+            add = int(sys.argv[1] + sys.argv[3])
+            print('{:d}'.format(add))
+        elif sys.argv[2] == '-':
+           sub = int(sys.argv[1 - 3])
+           print('{:d}'.format(a - b))
+        elif sys.argv[2] == '*':
+            mul = int(sys.argv[1 * 3])
             print('{:d}'.format(a * b))
-        elif len(sys.argv)[2] == '/':
+        elif sys.argv[2] == '/':
+            div = int(sys.argv[1 / 3])
             print('{:d}'.format(a / b))
         else:
             print('Unknown operator. Available operators: +, -, * and /')
