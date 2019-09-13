b95cefa625f877a6a41daaa8081d75fc6d645cd7
dabrabgellak <barbaracalle10i3@gmail.com>
2019-09-13 03:38:20 +0000

    added vars

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import calculator_1
+    from calculator_1 import add, sub, mul, div
     import sys
 
     argc = len(sys.argv)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
     
     if argc != 4:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         exit(1)
     else:
         if sys.argv[2] == '+':
-            add = int(sys.argv[1] + sys.argv[3])
-            print('{:d}'.format(add))
+            
+            print('{:d} + {:d} = {:d}'.format(a, b, add(a,b)))
         elif sys.argv[2] == '-':
-           sub = int(sys.argv[1 - 3])
-           print('{:d}'.format(a - b))
+
+           print('{:d} + {:d} = {:d}'.format(a, b, sub(a, b)))
         elif sys.argv[2] == '*':
-            mul = int(sys.argv[1 * 3])
-            print('{:d}'.format(a * b))
+
+                 print('{:d} + {:d} = {:d}'.format(a, b, mul(a, b)))
         elif sys.argv[2] == '/':
-            div = int(sys.argv[1 / 3])
-            print('{:d}'.format(a / b))
+
+            print('{:d} + {:d} = {:d}'.format(a, b, div(a, b)))
         else:
             print('Unknown operator. Available operators: +, -, * and /')
             exit(1)
-#        print('{:d} '.format(a <operator> <b> = <result>))
+
