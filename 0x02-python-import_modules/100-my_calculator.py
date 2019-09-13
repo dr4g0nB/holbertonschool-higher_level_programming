9f62b2a935ba8587b30d1713501d8098795399a3
dabrabgellak <barbaracalle10i3@gmail.com>
2019-09-13 22:29:47 +0000

    Doesn't do *

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,13 +4,14 @@ if __name__ == "__main__":
     import sys
 
     argc = len(sys.argv)
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    c = sys.argv[2]
 
     if argc != 4:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    c = sys.argv[2]
 
     if c != '+' and c != '-' and c != '/' and c != '*':
         print('Unknown operator. Available operators: +, -, * and /')
@@ -18,8 +19,8 @@ if __name__ == "__main__":
     elif c == '+':
         print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
     elif c == '-':
-        print('{:d} + {:d} = {:d}'.format(a, b, sub(a, b)))
+        print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
     elif c == '*':
-        print('{:d} + {:d} = {:d}'.format(a, b, mul(a, b)))
+        print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
     elif c == '/':
-        print('{:d} + {:d} = {:d}'.format(a, b, div(a, b)))
+        print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
