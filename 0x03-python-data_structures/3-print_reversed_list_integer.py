741e7ca36f7a77e495a4bf8615e5331dae47fd54
dabrabgellak <barbaracalle10i3@gmail.com>
2019-09-17 20:41:18 +0000

    3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,6 @@ def print_reversed_list_integer(my_list=[]):
 
     my_list.reverse()
     for trav in my_list:
+        if my_list is None:
+            return(my_list)
         print('{:d}'.format(trav))
