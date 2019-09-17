c868a82bd30a5c512b307bf01c0549b75d9565a8
dabrabgellak <barbaracalle10i3@gmail.com>
2019-09-17 21:44:09 +0000

    Rm elif and moved the copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
 
-    n_list = my_list.copy()
-
     if my_list is None:
         return(n_list)
-    elif idx < 0 or idx > len(my_list):
+
+    n_list = my_list.copy()
+    if idx < 0 or idx >= len(my_list):
         return(n_list)
-        
+
     for trav in n_list:
         n_list[idx] = element
         return(n_list)
