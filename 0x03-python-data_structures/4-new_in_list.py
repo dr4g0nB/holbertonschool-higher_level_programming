54c7b60e633a2c963ac675a532ab3273b89759d1
dabrabgellak <barbaracalle10i3@gmail.com>
2019-09-17 21:38:41 +0000

    without else

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -7,7 +7,7 @@ def new_in_list(my_list, idx, element):
         return(n_list)
     elif idx < 0 or idx > len(my_list):
         return(n_list)
-    else:
-        for trav in n_list:
-            n_list[idx] = element
-            return(n_list)
+        
+    for trav in n_list:
+        n_list[idx] = element
+        return(n_list)
