df873cbfc56347356e6e44b4c276f3fde12110a5
dabrabgellak <barbaracalle10i3@gmail.com>
2019-09-17 17:33:29 +0000

    Prints a list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Prints a list
+
+
+def print_list_integer(my_list=[]):
+
+    leng = len(my_list)
+    trav = 0
+
+    for trav in (my_list):
+        print('{}'.format(trav))
