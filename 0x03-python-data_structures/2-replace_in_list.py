d404f0cf8126180cc957bc8a0da18aa022b8eb45
dabrabgellak <barbaracalle10i3@gmail.com>
2019-09-17 20:21:36 +0000

    Added a return

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
 
-    leng = len(my_list)
-
     for trav in my_list:
 
         if idx is None:
             return(my_list)
-        elif idx > 0 or idx > leng:
+        elif idx < 0 or idx >= len(my_list):
             return(my_list)
-
         my_list[idx] = element
+        return(my_list)
