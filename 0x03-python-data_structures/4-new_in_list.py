0ee79fcf088d41b6688fb81239830c026932df8c
dabrabgellak <barbaracalle10i3@gmail.com>
2019-09-17 21:37:16 +0000

    changed the return value

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,9 +4,9 @@ def new_in_list(my_list, idx, element):
     n_list = my_list.copy()
 
     if my_list is None:
-        return(my_list)
+        return(n_list)
     elif idx < 0 or idx > len(my_list):
-        return(my_list)
+        return(n_list)
     else:
         for trav in n_list:
             n_list[idx] = element
