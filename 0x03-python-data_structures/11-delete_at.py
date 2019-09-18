cdcc5adebfb6bf561a004362f35578a2c58b7ea3
dabrabgellak <barbaracalle10i3@gmail.com>
2019-09-18 17:09:25 +0000

    Deletes an item

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,10 +2,11 @@
 def delete_at(my_list=[], idx=0):
 
     if my_list is None:
-        return(None)
+        return(my_list)
 
     if idx < 0 or idx > len(my_list):
         return(my_list)
-
-    for trav in my_list:
-        idx[]
+    else:
+        del my_list[idx]
+#        n_list += tra
+    return(my_list)
