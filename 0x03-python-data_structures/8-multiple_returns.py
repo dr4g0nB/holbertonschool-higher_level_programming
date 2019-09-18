f1f15030f61be3f9619efa57db85037fdfc1fb60
dabrabgellak <barbaracalle10i3@gmail.com>
2019-09-18 21:30:45 +0000

    is None

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,8 +4,8 @@ def multiple_returns(sentence):
     leng = len(sentence)
     buff = sentence[0]
 
-    if sentence is None:
-        buff == 0
+    if sentence == []:
+        buff is None
     else:
         buff == sentence[0]
     return(leng, buff)
