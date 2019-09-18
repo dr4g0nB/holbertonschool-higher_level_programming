79c6639e048f2fd8ae59ab51a5d7bab7dc7093bd
dabrabgellak <barbaracalle10i3@gmail.com>
2019-09-18 04:42:36 +0000

    an idea

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+
+    if my_list is None:
+        return(None)
+
+    if idx < 0 or idx > len(my_list):
+        return(my_list)
+
+    for trav in my_list:
+        idx[]
