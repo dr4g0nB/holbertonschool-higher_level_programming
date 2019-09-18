5fe31093a0c9656a5afcfc3cad576d35f3488ea4
dabrabgellak <barbaracalle10i3@gmail.com>
2019-09-18 20:28:45 +0000

    Finds multiples of 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,6 +2,12 @@
 def divisible_by_2(my_list=[]):
 
     if my_list is None:
-        return(None)
+        return(my_list)
 
-    n_list
+    n_list = []
+    for trav in my_list:
+        if trav % 2 == 0:
+            n_list += [True]
+        else:
+            n_list += [False]
+    return n_list
