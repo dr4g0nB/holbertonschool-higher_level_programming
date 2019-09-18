d814b2c7c033c54ac12f99b8d7ccd7164599ae92
dabrabgellak <barbaracalle10i3@gmail.com>
2019-09-18 21:38:35 +0000

    rm a declaration

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,10 +2,9 @@
 def multiple_returns(sentence):
 
     leng = len(sentence)
-    buff = sentence[0]
 
     if leng == 0:
         buff = None
     else:
-        buff == sentence[0]
+        buff = sentence[0]
     return(leng, buff)
