32d44806424eafbb2788c0d42610bf5b09ee8192
dabrabgellak <barbaracalle10i3@gmail.com>
2019-09-18 21:19:09 +0000

    Returns a tuple

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+
+    leng = len(sentence)
+    buff = sentence[0]
+
+    if leng == []:
+        buff == None
+    else:
+        buff == sentence[0]
+    return(leng, buff)
