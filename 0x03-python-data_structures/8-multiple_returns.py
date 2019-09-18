ac30496c5a07c3a2c12c74d71aa4cd96d8e073f4
dabrabgellak <barbaracalle10i3@gmail.com>
2019-09-18 21:36:20 +0000

    changed the condition

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,8 +4,8 @@ def multiple_returns(sentence):
     leng = len(sentence)
     buff = sentence[0]
 
-    if sentence == []:
-        buff is None
+    if leng == 0:
+        buff = None
     else:
         buff == sentence[0]
     return(leng, buff)
