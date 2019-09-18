e8f19108749418960036b2806be9ce466553dc5a
dabrabgellak <barbaracalle10i3@gmail.com>
2019-09-18 16:13:12 +0000

    Prints the matrix but not in [0]

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+
+    if matrix[0] == 0:
+        print()
+
+    for row in matrix:
+        if len(row) > 0:
+            for column in range(len(row)):
+                if column == len(row) - 1:
+                    print('{:d}'.format(row[column]))
+                else:
+                    print('{:d}'.format(row[column]), end=" ")
