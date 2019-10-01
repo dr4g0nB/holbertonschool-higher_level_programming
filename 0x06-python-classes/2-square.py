3e70703cb9c25a1cf0f24243355e9882200412f1
dabrabgellak <barbaracalle10i3@gmail.com>
2019-10-01 22:05:16 +0000

    Checks if size is an int or is less than zero

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+class Square:
+    """Defines a square class that will look for int in size
+    """
+    __size = "size"
+
+    def __init__(self, size=0):
+        """Checks if size is int or greater than zero
+            Raises:
+                TypeError: if size is not an integer
+                ValueError: if size is less than zero
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
