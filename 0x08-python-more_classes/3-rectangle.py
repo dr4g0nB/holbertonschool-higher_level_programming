8e4ca6a618a652da7b93e75fceea544bd99b202e
dabrabgellak <barbaracalle10i3@gmail.com>
2019-10-07 23:00:41 +0000

    Condition for perimeter method

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,62 @@
+#!usr/bin/python3
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        """  """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """ To retrieve width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ To set width """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """ To retrieve height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ To set height """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
+
+    def perimeter(self):
+        """ Returns the rectangle perimeter """
+        if self.width == 0 & self.height == 0:
+            self.perimeter = 0
+            return print()
+        else:
+            return (self.__height + self.__width) * 2
+
+    def area(self):
+        """ Returns the rectangle area """
+        return self.__width * self.__height
+        print('#')
+        print(str('#'))
+
+    def __str__(self):
+        """ Prints the rectangle """
+
+        str_rect = ''
+        i = 0
+        for i in range(self.__height):
+            if i == self.height - 1:
+                str_rect += str(self.__width * '#')
+            else:
+                str_rect += str(self.__width * '#') + '\n'
+        return str_rect
