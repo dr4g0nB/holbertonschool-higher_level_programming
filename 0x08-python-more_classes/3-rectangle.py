282ee669230db7d9180efb9c131faf65cc17d87f
dabrabgellak <barbaracalle10i3@gmail.com>
2019-10-09 21:39:02 +0000

    corrected the same as 2

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,4 +1,9 @@
 #!usr/bin/python3
+"""
+    module `rectangle`
+"""
+
+
 class Rectangle:
     def __init__(self, width=0, height=0):
         """  """
@@ -17,7 +22,6 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-
         self.__width = value
 
     @property
@@ -32,26 +36,24 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-
         self.__height = value
 
     def perimeter(self):
         """ Returns the rectangle perimeter """
         if self.width == 0 & self.height == 0:
-            self.perimeter = 0
+            return 0
             return print()
         else:
             return (self.__height + self.__width) * 2
 
     def area(self):
         """ Returns the rectangle area """
-        return self.__width * self.__height
+        return self.width * self.height
 #        print('#')
 #        print(str('#'))
 
     def __str__(self):
         """ Prints the rectangle """
-
         str_rect = ''
         i = 0
         for i in range(self.__height):
