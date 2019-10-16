93b4f5790a7dcc9fba20d2bfd45b407ebfc0efd3
dabrabgellak <barbaracalle10i3@gmail.com>
2019-10-16 05:17:13 +0000

    changed the order of veri

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -35,6 +35,6 @@ class Rectangle(BaseGeometry):
     """ Subclass """
     def __init__(self, width, height):
         self.integer_validator('width', width)
-        self.__width = width
         self.integer_validator('height', height)
         self.__height = height
+        self.__width = width
