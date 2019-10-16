6103d42aa821de793c630a348c70738e26a9be1e
dabrabgellak <barbaracalle10i3@gmail.com>
2019-10-16 23:18:42 +0000

    Reads a .txt

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-""" Module """
-
-
 def read_file(filename=""):
-    """ Reads a txt file and prints it to stdout
-    """
-    with open(filename, encoding='utf-8') as op_r:
-        re = op_r.read()
-        print('{:s}'.format(re), end="")
+    """  """
+
+    with open(filename, encoding="utf-8") as op:
+        re = op.read()
+        print(re, end="")
