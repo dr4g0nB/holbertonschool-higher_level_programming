19a5213c987a657a10406fd18dcd155d020a13d0
dabrabgellak <barbaracalle10i3@gmail.com>
2019-10-16 23:03:56 +0000

    comillas sencillas

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,6 +2,6 @@
 def read_file(filename=""):
     """ Reads a txt file and prints it to stdout """
 
-    with open(filename, encoding="utf-8") as op_r:
+    with open(filename, encoding='utf-8') as op_r:
         re = op_r.read()
         print(re, end="")
