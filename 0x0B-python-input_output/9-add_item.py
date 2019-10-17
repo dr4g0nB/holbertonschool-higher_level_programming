4250877bc43ea41a296b75eec67452ff9a82aa3a
dabrabgellak <barbaracalle10i3@gmail.com>
2019-10-17 03:33:01 +0000

    Adds all args

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import os
+import sys
+
+filename = "add_item.json"
+os.path.isfile(add_item.json)
+exists(add_item.json)
+open(add_item.json, 'w')
+load_from_json_file(add_item.json)
+add_item.json.append(sys.argv)
+save_to_json_file()
