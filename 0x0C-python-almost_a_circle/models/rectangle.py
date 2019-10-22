16497235876b710c0881e7133a75a4e1b39661cf
dabrabgellak <barbaracalle10i3@gmail.com>
2019-10-22 21:32:24 +0000

    dk

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,6 +7,7 @@ class Rectangle(Base):
     """ Rectangle """
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """ Initialization """
         super().__init__(id)
         self.width = width
         self.height = height
@@ -15,10 +16,12 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """ width property """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """ width setter """
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -27,10 +30,12 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """ height property """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """ height setter """
         if type(value) != int:
             raise TypeError("height must be an integer")
         if value <= 0:
@@ -39,10 +44,12 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """ x property """
         return self.__x
 
     @x.setter
     def x(self, value):
+        """ x setter """
         if type(value) != int:
             raise TypeError("x  must be an integer")
         if value < 0:
@@ -51,10 +58,12 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """ y property """
         return self.__y
 
     @y.setter
     def y(self, value):
+        """ y setter """
         if type(value) != int:
             raise TypeError("y must be an integer")
         if value < 0:
@@ -110,5 +119,6 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         """ Returns the dictionary representation of Rectangle """
-        rect_dict = {'id': self.id , 'width':self.__width , 'height':self.__height , 'x':self.__x , 'y':self.__y}
+        rect_dict = {'id': self.id, 'width': self.__width,
+                     'height': self.__height, 'x': self.__x, 'y': self.__y}
         return rect_dict
