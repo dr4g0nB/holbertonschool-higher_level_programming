e94ef8a9033725538ac5c2d967792aa1c2e61258
dabrabgellak <barbaracalle10i3@gmail.com>
2019-10-22 14:19:29 +0000

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-from models.rectangle import Rectangle 
+from models.base import Base
+from models.rectangle import Rectangle
 """ Inherits from Rectangle """
 
 
@@ -7,16 +8,50 @@ class Square(Rectangle):
     """ Square """
     def __init__(self, size, x=0, y=0, id=None):
         """ Initialization """
-        super().__init__(id)
-        super().__init__(x)
-        super().__init__(y)
-        super().__init__(width)
-        super().__init__(height)
-        self.__height = size
-        self.__width = size
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    @property
+    def size(self):
+        """ size property """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ size's setter """
+        self.width = value
+        self.height = value
 
     def __str__(self):
-        """ Return
-                Square dimensions
-        """
-        return('[Square] ({}) {}/{} - {}'.format(self.id, self.__x, self.__y, self.size))
+        """Returns Square dimensions """
+        return('[Square] ({}) {}/{} - {}'.format(self.id, self.x,
+                                                 self.y, self.width))
+
+    def update(self, *args, **kwargs):
+        """ Assigns attributes """
+        """ args """
+        for posit, trav_args in enumerate(args):
+            if posit == 0:
+                self.id = trav_args
+            if posit == 1:
+                self.size = trav_args
+            if posit == 2:
+                self.x = trav_args
+            if posit == 3:
+                self.y = trav_args
+
+        """ kwargs """
+        for k_value, v_value in kwargs.items():
+            if k_value == "id":
+                self.id = v_value
+            if k_value == "size":
+                self.size = v_value
+            if k_value == "x":
+                self.x = v_value
+            if k_value == "y":
+                self.y = v_value
+
+    def to_dictionary(self):
+        """ Returns a dictionary representation of Square """
+        sq_dict = {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
+        return sq_dict
