fc7035754896877e50a97385d7e8296efd0a2abc
dabrabgellak <barbaracalle10i3@gmail.com>
2019-11-26 21:38:48 +0000

    Corrected

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Lists all cities contained in the database hbtn_0d_usa
-SELECT states.id, cities.name, states.name
+SELECT cities.id, cities.name, states.name
 FROM states
 INNER JOIN cities
 ON states.id = cities.id
