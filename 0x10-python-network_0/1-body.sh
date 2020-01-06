ae8588e75620f7051185a773677bcc4d02525591
dabrabgellak <barbaracalle10i3@gmail.com>
2020-01-06 21:46:02 +0000

    Different

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a GET request to the URL, and displays the body of the response.
-curl -sILf "$1" -X GET -w "%{http_code}" http://1ef36dcc94f7.38.hbtn-cod.io:5000
+curl -sILf -X GET "$1"
