4388025e19d87419a44c11d1a9475b73a15df00d
dabrabgellak <barbaracalle10i3@gmail.com>
2020-01-13 20:17:25 +0000

    please

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,11 +3,12 @@
 import urllib
 import urllib.request
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as retrieved_data:
-    data = retrieved_data.read()
-    html = data.decode("UTF-8")
-    print("Body response:")
-    print("\t- type: ", type(data))
-    print("\t- content:", data)
-    print("\t- utf8 content: ", html)
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as retrieved_data:
+        data = retrieved_data.read()
+        html = data.decode()
+        print("Body response:")
+        print("\t- type:", type(data))
+        print("\t- content:", data)
+        print("\t- utf8 content:", html)
 
