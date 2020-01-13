686142f4aff6f4b21a6f670fd91c51d0f650e42b
dabrabgellak <barbaracalle10i3@gmail.com>
2020-01-13 20:09:12 +0000

    without format

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ with urllib.request.urlopen('https://intranet.hbtn.io/status') as retrieved_data
     data = retrieved_data.read()
     html = data.decode("UTF-8")
     print("Body response:")
-    print("\t - type: {}".format(type(data)))
-    print("\t - content: {}".format(data))
-    print("\t - utf8 content: {}".format(html))
+    print("\t- type: ", type(data))
+    print("\t- content:", data)
+    print("\t- utf8 content: ", html)
 
