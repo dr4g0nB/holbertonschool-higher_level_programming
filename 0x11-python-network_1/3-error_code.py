dad6837976bd1273f32d9fc78fa8c8da16e3022a
dabrabgellak <barbaracalle10i3@gmail.com>
2020-01-13 23:54:14 +0000

    task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import sys
+import urllib
+import urllib.request
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as url_data:
+            r_url = url_data.read()
+            print(r_url.decode("UTF-8"))
+    except urllib.error.HTTPError as error:
+       print("Error code: {}".format(error.code))
