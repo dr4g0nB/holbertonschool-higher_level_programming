267433c120aae8d1527cd0592ae8b46393f36377
dabrabgellak <barbaracalle10i3@gmail.com>
2020-01-14 00:35:49 +0000

    task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    retrieved_url = requests.get(url)
+    ret_url = retrieved_url.headers
+    print(ret_url['X-Request-Id'])
