ffd652b0675181f931f2078a85fb5a6b779c57bc
bxrbxrx <barbaracalle10i3@gmail.com>
2020-02-04 13:58:25 -0500

    semistandard happy

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 
-let languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 let trav = 0;
 
 while (trav < languages.length) {
-    console.log(languages[trav]);
-    trav++;
+  console.log(languages[trav]);
+  trav++;
 }
