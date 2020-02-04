27932d275409725dfe2207557f683e10b8cadd00
bxrbxrx <barbaracalle10i3@gmail.com>
2020-02-04 16:04:02 -0500

    semistandar happy

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,11 +2,11 @@
 
 const arg = parseInt(process.argv[2]);
 function factorialofarg (arg) {
-    if (!(arg)) {
-        return 1;
-    }
-    arg *= factorialofarg(arg - 1);
-    return arg;
+  if (!(arg)) {
+    return 1;
+  }
+  arg *= factorialofarg(arg - 1);
+  return arg;
 }
 
 console.log(factorialofarg(arg));
