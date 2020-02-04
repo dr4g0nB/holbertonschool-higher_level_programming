79bac5d151e5bb16a3f7c654bc34a75bd1254b4e
bxrbxrx <barbaracalle10i3@gmail.com>
2020-02-04 16:04:32 -0500

    semistandar happy

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 function add (a,b) {
-    const number = Number(process.argv[2]);
-    const numbertoadd = Number(process.argv[3]);
-    const result = number + numbertoadd;
-    console.log(result);
+  const number = Number(process.argv[2]);
+  const numbertoadd = Number(process.argv[3]);
+  const result = number + numbertoadd;
+  console.log(result);
 }
 add();
