1a369f9cd2d1ccf44a6e7ae685cd833b38fbee17
bxrbxrx <barbaracalle10i3@gmail.com>
2020-02-04 13:59:39 -0500

    semistandard happy

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 if (parseInt(process.argv[2])) {
-    let multi = 0;
-    while (multi < process.argv[2]) {
-        console.log('C is fun');
-        multi++;
+  let multi = 0;
+  while (multi < process.argv[2]) {
+    console.log('C is fun');
+    multi++;
     } 
 } else {
-    console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 }
