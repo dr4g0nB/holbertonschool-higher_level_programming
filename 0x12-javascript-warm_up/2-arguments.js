10420d963c513220be560310a3e0a0c9ec886cf5
bxrbxrx <barbaracalle10i3@gmail.com>
2020-02-04 19:33:12 -0500

    Prints

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+
+if (ProcessingInstruction.argv.length <= 2) {
+  console.log('No argument');
+} else if (ProcessingInstruction.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
