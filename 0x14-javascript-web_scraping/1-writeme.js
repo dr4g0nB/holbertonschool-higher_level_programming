9092063f11e1168a6ca68fb52a36ced85bf904eb
dabrabgellak <barbaracalle10i3@gmail.com>
2020-02-11 17:19:57 +0000

    semistandard happy

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
-const fs = require['fs'];
-const file_path = process.argv[2];
+const fs = require('fs');
+const filePath = process.argv[2];
 const stringTowrite = process.argv[3];
 
-fs.writeFile(file_path, stringTowrite, "utf-8", function(error) {
+fs.writeFile(filePath, stringTowrite, 'utf-8', function (error) {
   if (error) {
     console.error(error);
-  } else { 
+  } else {
     console.log(stringTowrite);
   }
 });
