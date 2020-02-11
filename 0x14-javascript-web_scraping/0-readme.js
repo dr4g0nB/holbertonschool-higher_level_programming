c66aa46c2e8e0b4c8da67e95317b24a38e3406f2
dabrabgellak <barbaracalle10i3@gmail.com>
2020-02-11 17:38:01 +0000

    funciona

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,7 +5,7 @@ const argv = process.argv[2];
 
 fs.readFile(argv, 'utf-8', function (err, data) {
   if (err) {
-    process.stdout.write(err);
+    return console.log(err);
   } else {
     process.stdout.write(data);
   }
