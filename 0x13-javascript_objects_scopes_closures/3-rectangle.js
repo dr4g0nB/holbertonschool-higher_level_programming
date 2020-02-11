b938303d23dbaecde0476f1218b44c0573934df0
dabrabgellak <barbaracalle10i3@gmail.com>
2020-02-11 04:18:48 +0000

    prints

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    } else {
+      const emptyObj = {};
+    }
+
+    Rectangle.prototype.print = function () {
+      for (let alto = 0; alto < h; alto++) {
+        let printRectangle = '';
+        for (let ancho = 0; ancho < w; ancho++) {
+          printRectangle += 'X';
+        }
+        console.log(printRectangle);
+      }
+    };
+  }
+};
