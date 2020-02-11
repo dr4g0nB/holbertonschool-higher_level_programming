6884aa8154a8b3f4849effea8d81ee9501cac9ce
dabrabgellak <barbaracalle10i3@gmail.com>
2020-02-11 03:59:03 +0000

    2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,25 +1,12 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-<<<<<<< HEAD
   constructor (w, h) {
-    if ((w <= 0) || (h <= 0)) {
-      let empty_obj = new Object();
-    } else {
+    if ((w > 0) && (h > 0)) {
       this.width = w;
       this.height = h;
+    } else {
+      const emptyObj = {};
     }
   }
 };
-=======
-    constructor (w, h) {
-      if ((w <= 0) || (h <= 0)) {
-        let empty_obj = new Object();
-      }
-      else {
-        this.width = w;
-        this.height = h;
-      }
-    }
-  };
->>>>>>> c4544a59fd5aec43ec3b8016b0b25e7bc499d4d7
