da0f0bfefb7acc43528de538c73857e5687d870f
dabrabgellak <barbaracalle10i3@gmail.com>
2020-02-11 17:18:16 +0000

    semistandard happy

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-let fs = require("fs");
+const fs = require('fs');
 const argv = process.argv[2];
 
-fs.readFile(argv, "utf-8", function(error, data) {
+fs.readFile(argv, 'utf-8', function (error, data) {
   if (error) {
     console.error(error);
   } else {
