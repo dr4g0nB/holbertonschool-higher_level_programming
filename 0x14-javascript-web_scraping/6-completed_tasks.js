24202977e3458f216255f93563f94b1cdde860f3
dabrabgellak <barbaracalle10i3@gmail.com>
2020-02-12 23:59:03 +0000

    Might work

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,10 +3,20 @@
 const request = require('request');
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
-request.get(url, function (err, response, body){
+request.get(url, function (err, response, body) {
   if (err) {
     return console.log(err);
-  } else {
-    console.log(JSON.parse(body).userId);
+  }
+
+  if (response) {
+    const data = JSON.parse(body);
+    const todosDict = {};
+    for (const travDictUser of data) {
+      if (travDictUser.completed) todosDict[travDictUser.userId] = 0;
+    }
+    for (const travDictCompleted of data) {
+      if (travDictCompleted.completed) todosDict[travDictCompleted.userId] += 1;
+    }
+    console.log(todosDict);
   }
 });
