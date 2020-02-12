2af0bcdf9399ae5f79cc45ea85927873ac4b50f6
dabrabgellak <barbaracalle10i3@gmail.com>
2020-02-12 00:10:02 +0000

    halfway

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+const argv = process.argv[2];
+
+request(argv, function(err, response, body) {
+  if (err) {
+    return console.log(err);
+  } else {
+    for () {}
+    console.log(JSON.parse(body).id);
+  }
+});
