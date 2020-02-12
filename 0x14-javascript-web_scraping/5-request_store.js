7bac5e660a253333527aa865d5aa0f87826fa33e
dabrabgellak <barbaracalle10i3@gmail.com>
2020-02-12 00:08:08 +0000

    no da

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request');
+const argv = process.argv[2];
+const file = process.argv[3];
+
+request(argv, function(err, body) {
+  if (err) {
+    return console.log(err);
+  } else {
+    fs.write(file, 'utf-8', body);
+  }
+});
